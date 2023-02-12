b8d1f7e9459ab9e47883f8b2fb8de19437141161
unknown <muahackff@gmail.com>
2023-02-12 18:22:24 +0700

    gamemod

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Gia Hân';
+const giftUrl = '';
+const eventName = 'Chúc Bé Valentine';
+const titleCard = 'Tặng bé iu';
+const contentCard = 'Chúc honey của anh valetine tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau \n. Và nhớ kiểm tra cóp xe nha, có điều bất ngờ cho bé đó hihi';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'you1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
