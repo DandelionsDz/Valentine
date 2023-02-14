5ffcafca901e7e1f3f26be5c7a9472146f42bc57
gamemodpro503 <90510111+DandelionsDz@users.noreply.github.com>
2023-02-14 19:13:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
-const nameGirl = 'Gia Hân';
+const nameGirl = 'Trinh Trinh';
 const giftUrl = '';
 const eventName = 'Chúc Bé Valentine';
 const titleCard = 'Tặng bé iu';
-const contentCard = 'Chúc honey của anh valetine tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau \n. Và nhớ kiểm tra cóp xe nha, có điều bất ngờ cho bé đó hihi';
+const contentCard = 'Chúc honey của anh valetine tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'you1.png';
+const giftImage = 'you.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
